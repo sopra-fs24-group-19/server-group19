6ad27890a136d79f9ded6fcb957829da767b6c81
Holwy <francesco.manzionna@gmail.com>
2024-04-08 22:46:44 +0200

    Completed US1, US2 #25 #39

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/UserController.java
@@ -5,6 +5,7 @@ import ch.uzh.ifi.hase.soprafs24.rest.dto.UserGetDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.UserPostDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.UserPutDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.RatingPostDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.UserEditDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs24.service.UserService;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class UserController {
@@ -22,36 +25,40 @@ public class UserController {
     this.userService = userService;
   }
 
-    @PostMapping("/users")
-    @ResponseStatus(HttpStatus.CREATED)
-    @ResponseBody
-    public UserGetDTO createUser(@RequestBody UserPostDTO userPostDTO) {
-        User userInput = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
-        User createdUser = userService.createUser(userInput);
-        return DTOMapper.INSTANCE.convertEntityToUserGetDTO(createdUser);
-    }
+  @PostMapping("/users")
+  @ResponseStatus(HttpStatus.CREATED)
+  @ResponseBody
+  public UserGetDTO createUser(@RequestBody UserPostDTO userPostDTO, HttpServletResponse response) {
+    User userInput = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
+    User createdUser = this.userService.createUser(userInput);
+    response.addHeader("Authorization", createdUser.getToken());
+    return DTOMapper.INSTANCE.convertEntityToUserGetDTO(createdUser);
+  }
 
-    @PutMapping("/users")
+    @PutMapping("/users") //QUESTION the path should be /login
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public UserGetDTO loginUser(@RequestBody UserPutDTO userPutDTO) {
-        // TODO: implement
-        User user = DTOMapper.INSTANCE.convertUserPutDTOtoEntity(userPutDTO);
-        return DTOMapper.INSTANCE.convertEntityToUserGetDTO(user);
+    public UserGetDTO loginUser(@RequestBody UserPutDTO userPutDTO, HttpServletResponse response) {
+      User userInput = DTOMapper.INSTANCE.convertUserPutDTOtoEntity(userPutDTO);
+      User userRetrieved = this.userService.login(userInput);
+      UserGetDTO userSentToClient = DTOMapper.INSTANCE.convertEntityToUserGetDTO(userRetrieved);
+      response.addHeader("Authorization", userRetrieved.getToken());
+      return userSentToClient;
     }
 
     @PutMapping("/logout")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @ResponseBody
-    public void logoutUser(@RequestBody long id) {
-        // TODO: implement
+    public void logout(@RequestHeader("Authorization") String token) {
+      this.userService.logOut(token);
     }
 
-    @PutMapping("/users/{id}")
+    @PutMapping("/users/{id}") //QUESTION This logically conflicts with PUT login, even though it would work
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @ResponseBody
-    public void updateUser(@PathVariable("id") long id) {
-        // TODO: implement
+    public void editProfile(@PathVariable Long id, @RequestBody UserEditDTO userEditDTO,
+        @RequestHeader("Authorization") String token) {
+      User userWithPendingChanges = DTOMapper.INSTANCE.convertUserEditDTOToEntity(userEditDTO);
+      this.userService.editProfile(userWithPendingChanges, token, id);
     }
 
   @GetMapping("/users/{userId}")
--- src/main/java/ch/uzh/ifi/hase/soprafs24/repository/UserRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/UserRepository.java
@@ -10,5 +10,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
   User findByUsername(String username);
 
+  User findUserById(Long id);
+
+  User findUserByToken(String token);
+
 
 }
