920ae00441acc5103a5f4471423abf43f20e9dac
dana-jpg <dana.rapp@uzh.ch>
2024-04-17 15:00:18 +0200

    remove tests for login

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/UserServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/UserServiceIntegrationTest.java
@@ -34,44 +34,6 @@ public class UserServiceIntegrationTest {
     userRepository.deleteAll();
   }
 
-  @Test
-  public void createUser_validInputs_success() {
-    // given
-    assertNull(userRepository.findByUsername("testUsername"));
 
-    User testUser = new User();
-    testUser.setName("testName");
-    testUser.setUsername("testUsername");
-    testUser.setPassword("testPassword");
 
-    // when
-    User createdUser = userService.createUser(testUser);
-
-    // then
-    assertEquals(testUser.getId(), createdUser.getId());
-    assertEquals(testUser.getName(), createdUser.getName());
-    assertEquals(testUser.getUsername(), createdUser.getUsername());
-    assertNotNull(createdUser.getToken());
-  }
-
-  @Test
-  public void createUser_duplicateUsername_throwsException() {
-    assertNull(userRepository.findByUsername("testUsername"));
-
-    User testUser = new User();
-    testUser.setName("testName");
-    testUser.setUsername("testUsername");
-      testUser.setPassword("testPassword");
-    User createdUser = userService.createUser(testUser);
-
-    // attempt to create second user with same username
-    User testUser2 = new User();
-
-    // change the name but forget about the username
-    testUser2.setName("testName2");
-    testUser2.setUsername("testUsername");
-
-    // check that an error is thrown
-    assertThrows(ResponseStatusException.class, () -> userService.createUser(testUser2));
-  }
 }
